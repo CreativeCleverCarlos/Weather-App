5fc929c787e7bcfaf9bc161ce86db429d99a2bca
Carlos Campbells Laptop <carlos.g.campbell@gmail.com>
2024-04-12 14:11:55 -0400

    fetched and collect all necessary data to display to user

--- main.js
+++ main.js
@@ -10,8 +10,19 @@ console.log("Hello World")
 async function getWeather() {
     const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=831cf2b10ac1459e9a3154716241204&q=${inputHolder[0]}&aqi=no`) ;
     const weatherData = await response.json();
-    let test = weatherData.location
-    console.log(test)
+    let location = weatherData.location.name;
+    let region = weatherData.location.region;
+    let country = weatherData.location.region;
+    let localTime = weatherData.location.localtime;
+    let temp_celsius = weatherData.current.temp_c;
+    let temp_fehrenheit = weatherData.current.temp_f;
+    let condition = weatherData.current.condition.text;
+    let humidity = weatherData.current.humidity;
+    let windSpeed = weatherData.current.wind_kph;
+    let precipitation = weatherData.current.precip_mm;
+    return {
+        location, region, country, localTime, temp_celsius, temp_fehrenheit, condition, humidity, windSpeed, precipitation
+    }
   }
 
 
@@ -33,5 +44,4 @@ inputLocation("brampton")
 
 getWeather()
 
-
-
+console.log(getWeather())
